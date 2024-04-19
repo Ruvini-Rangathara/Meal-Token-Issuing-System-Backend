fa876f6c1dd4d93c853dc9f41cee6ac57af0cf4e
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-04-19 18:56:38 +0530

    implement meal controller

--- src/controller/meal.controller.ts
+++ src/controller/meal.controller.ts
@@ -13,21 +13,48 @@ export class MealController {
 
 
   @Get()
-  async findAll(): Promise<Meal[]> {
-
+  async findAll(): Response{
+    try{
+      let mealDTO = await this.mealService.findAll();
+      if (mealDTO.length > 0) {
+        return new Response(HttpStatus.OK, "Meals found", mealDTO);
+      } else {
+        return new Response(HttpStatus.NO_CONTENT, "No meals found", []);
+      }
+    }catch (error) {
+      console.error("Error getting meals:", error);
+      return new Response(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting meals", error);
+    }
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: any): Promise<Meal> {
-
+  async findOne(@Param('id') id: any): Response {
+    try{
+      let mealDTO = await this.mealService.findOne(id);
+      if (mealDTO) {
+        return new Response(HttpStatus.OK, "Meal found", mealDTO);
+      } else {
+        return new Response(HttpStatus.NO_CONTENT, "No meal found", []);
+      }
+    }catch (error) {
+      console.error("Error getting meal:", error);
+      return new Response(HttpStatus.INTERNAL_SERVER_ERROR, "Error getting meal", error);
+    }
   }
 
   @Post()
-  async create(@Body() mealData: Partial<Meal>): Promise<Meal> {
-
+  async create(@Body() mealData: Partial<Meal>): Response {
+    try{
+      if(await this.mealService.findOne(mealData.id)){
+        return new Response(HttpStatus.CONFLICT, "Meal already exists", []);
+      }
+      let mealDTO = await this.mealService.create(mealData);
+      return new Response(HttpStatus.CREATED, "Meal created", mealDTO);
+    }catch (error) {
+      console.error("Error creating meal:", error);
+      return new Response(HttpStatus.INTERNAL_SERVER_ERROR, "Error creating meal", error);
+    }
   }
-
-
 
 
 }
