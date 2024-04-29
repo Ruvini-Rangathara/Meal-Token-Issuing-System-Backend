b058eda315e479486c9c54d3a6e2532cf5a66ee0
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-04-29 23:40:52 +0530

    add cors filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(cors({
+    origin: ['http://localhost:5173'], // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow sending cookies
+  }));
+
   await app.listen(3000);
 }
 bootstrap();
